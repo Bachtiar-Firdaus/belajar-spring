f42bfd73fb1468ed9252734e731df7bd9981eba6
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-14 19:38:00 +0700

    Belajar Spring Dasar 5 # Duplicate Bean

---
+++ belajar-spring-dasar/src/main/java/bachtiar/firdaus/belajar_spring_dasar/DuplicateConfiguration.java
@@ -0,0 +1,20 @@
+package bachtiar.firdaus.belajar_spring_dasar;
+
+import bachtiar.firdaus.belajar_spring_dasar.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DuplicateConfiguration {
+
+    @Bean
+    public Foo foo1(){
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo2(){
+        return new Foo();
+    }
+
+}
