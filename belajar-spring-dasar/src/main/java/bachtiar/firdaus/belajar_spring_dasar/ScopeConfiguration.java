4f60120d295ecede977855c47c271f50adc3d875
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-14 21:38:06 +0700

    Belajar Spring Dasar 12 # Scope

---
+++ belajar-spring-dasar/src/main/java/bachtiar/firdaus/belajar_spring_dasar/ScopeConfiguration.java
@@ -0,0 +1,35 @@
+package bachtiar.firdaus.belajar_spring_dasar;
+
+import bachtiar.firdaus.belajar_spring_dasar.data.Bar;
+import bachtiar.firdaus.belajar_spring_dasar.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Slf4j
+@Configuration
+public class ScopeConfiguration {
+
+    @Bean
+    @Scope("prototype")
+    public Foo foo(){
+        log.info("Create new Foo");
+        return new Foo();
+    }
+
+//    @Bean
+//    public CustomScopeConfigurer customScopeConfigurer(){
+//        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+//        configurer.addScope("doubleton", new DoubletonScope());
+//        return configurer;
+//    }
+//
+//    @Bean
+//    @Scope("doubleton")
+//    public Bar bar(){
+//        log.info("Create new Bar");
+//        return new Bar();
+//    }
+}
