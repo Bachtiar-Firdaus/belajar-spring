4bc6e9b35e87bca17069bf58beca9e3b10e67475
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-16 13:45:03 +0700

    Belajar Spring Dasar 33 # Spring Boot Application

---
+++ belajar-spring-dasar/src/test/java/bachtiar/firdaus/belajar_spring_dasar/application/FooApplicationTest.java
@@ -0,0 +1,22 @@
+package bachtiar.firdaus.belajar_spring_dasar.application;
+
+import bachtiar.firdaus.belajar_spring_dasar.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = FooApplication.class)
+class FooApplicationTest {
+
+    @Autowired
+    Foo foo;
+
+    @Test
+    void testSpringBoot() {
+        Assertions.assertNotNull(foo);
+    }
+}
