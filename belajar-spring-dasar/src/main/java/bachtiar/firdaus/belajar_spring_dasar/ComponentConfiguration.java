89de67091b70af1948ac3a38d21f5ad56341a601
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-16 11:01:56 +0700

    Belajar Spring Dasar 18 # Component

---
+++ belajar-spring-dasar/src/main/java/bachtiar/firdaus/belajar_spring_dasar/ComponentConfiguration.java
@@ -0,0 +1,16 @@
+package bachtiar.firdaus.belajar_spring_dasar;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+//import bachtiar.firdaus.belajar_spring_dasar.MultiFoo;
+
+@Configuration
+@ComponentScan(basePackages = {
+//        "bachtiar.firdaus.belajar_spring_dasar.repository",
+        "bachtiar.firdaus.belajar_spring_dasar.service",
+//        "bachtiar.firdaus.belajar_spring_dasar.configuration",
+})
+//@Import(MultiFoo.class)
+public class ComponentConfiguration {
+}
---
+++ belajar-spring-dasar/src/main/java/bachtiar/firdaus/belajar_spring_dasar/service/ProductService.java
@@ -0,0 +1,26 @@
+package bachtiar.firdaus.belajar_spring_dasar.service;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+// @Scope("prototype")
+// @Lazy
+@Component
+public class ProductService {
+
+//    @Getter
+//    private ProductRepository productRepository;
+//
+//    @Autowired
+//    public ProductService(ProductRepository productRepository){
+//        this.productRepository = productRepository;
+//    }
+//
+//    public ProductService(ProductRepository productRepository, String name){
+//        this.productRepository = productRepository;
+//    }
+
+}
