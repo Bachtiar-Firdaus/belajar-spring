07be80c14124882a7b516eed9b71492cf7c138a8
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-16 19:47:54 +0700

    Belajar Spring Config Properties 15 # Collection Configuration Properties

--- belajar-spring-config-properties/src/test/java/bachtiar/firdaus/belajar_spring_config_properties/configurationproperties/ConfigurationPropertiesTest.java
+++ belajar-spring-config-properties/src/test/java/bachtiar/firdaus/belajar_spring_config_properties/configurationproperties/ConfigurationPropertiesTest.java
@@ -50,28 +50,28 @@ public class ConfigurationPropertiesTest {
         Assertions.assertEquals("belajar", properties.getDatabase().getDatabase());
         Assertions.assertEquals("jdbc:contoh", properties.getDatabase().getUrl());
     }
-//
-//    @Test
-//    void testCollection() {
-//        Assertions.assertEquals(Arrays.asList("products", "customers", "categories"), properties.getDatabase().getWhitelistTables());
-//        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("products"));
-//        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("categories"));
-//        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("customers"));
-//    }
-//
-//    @Test
-//    void testEmbeddedCollection() {
-//        Assertions.assertEquals("default", properties.getDefaultRoles().get(0).getId());
-//        Assertions.assertEquals("Default Role", properties.getDefaultRoles().get(0).getName());
-//        Assertions.assertEquals("guest", properties.getDefaultRoles().get(1).getId());
-//        Assertions.assertEquals("Guest Role", properties.getDefaultRoles().get(1).getName());
-//
-//        Assertions.assertEquals("admin", properties.getRoles().get("admin").getId());
-//        Assertions.assertEquals("Admin Role", properties.getRoles().get("admin").getName());
-//        Assertions.assertEquals("finance", properties.getRoles().get("finance").getId());
-//        Assertions.assertEquals("Finance Role", properties.getRoles().get("finance").getName());
-//    }
-//
+
+    @Test
+    void testCollection() {
+        Assertions.assertEquals(Arrays.asList("products", "customers", "categories"), properties.getDatabase().getWhitelistTables());
+        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("products"));
+        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("categories"));
+        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("customers"));
+    }
+
+    @Test
+    void testEmbeddedCollection() {
+        Assertions.assertEquals("default", properties.getDefaultRoles().get(0).getId());
+        Assertions.assertEquals("Default Role", properties.getDefaultRoles().get(0).getName());
+        Assertions.assertEquals("guest", properties.getDefaultRoles().get(1).getId());
+        Assertions.assertEquals("Guest Role", properties.getDefaultRoles().get(1).getName());
+
+        Assertions.assertEquals("admin", properties.getRoles().get("admin").getId());
+        Assertions.assertEquals("Admin Role", properties.getRoles().get("admin").getName());
+        Assertions.assertEquals("finance", properties.getRoles().get("finance").getId());
+        Assertions.assertEquals("Finance Role", properties.getRoles().get("finance").getName());
+    }
+
 //    @Test
 //    void testDuration() {
 //        Assertions.assertEquals(Duration.ofSeconds(10), properties.getDefaultTimeout());
