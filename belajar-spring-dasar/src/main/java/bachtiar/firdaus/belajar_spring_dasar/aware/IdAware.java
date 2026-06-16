022de31b82553b2a6e793a8d9c9b3d9575f01056
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-16 12:09:58 +0700

    Belajar Spring Dasar 27 # Bean Post Processor

---
+++ belajar-spring-dasar/src/main/java/bachtiar/firdaus/belajar_spring_dasar/aware/IdAware.java
@@ -0,0 +1,9 @@
+package bachtiar.firdaus.belajar_spring_dasar.aware;
+
+public interface IdAware {
+
+    void setId(String id);
+
+    String getId();
+
+}
---
+++ belajar-spring-dasar/src/main/java/bachtiar/firdaus/belajar_spring_dasar/data/Car.java
@@ -0,0 +1,17 @@
+package bachtiar.firdaus.belajar_spring_dasar.data;
+
+import bachtiar.firdaus.belajar_spring_dasar.aware.IdAware;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements IdAware {
+
+    @Getter
+    private String id;
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+}
