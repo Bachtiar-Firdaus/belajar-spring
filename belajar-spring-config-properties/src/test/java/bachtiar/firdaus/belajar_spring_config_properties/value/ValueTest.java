77a1f6acf38e7d43646ff717821f36791b7ced03
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-16 17:41:29 +0700

    Belajar Spring Config Properties 8 # Value

---
+++ belajar-spring-config-properties/src/test/java/bachtiar/firdaus/belajar_spring_config_properties/value/ValueTest.java
@@ -0,0 +1,62 @@
+package bachtiar.firdaus.belajar_spring_config_properties.value;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+@SpringBootTest(classes = ValueTest.TestApplication.class)
+public class ValueTest {
+
+    @Autowired
+    private TestApplication.ApplicationProperties properties;
+
+    @Autowired
+    private TestApplication.SystemProperties systemProperties;
+
+    @Test
+    void testValue() {
+        Assertions.assertEquals("belajar-spring-config-properties", properties.getName());
+        Assertions.assertEquals(1, properties.getVersion());
+        Assertions.assertEquals(false, properties.isProductionMode());
+    }
+
+    @Test
+    void testSystemProperties() {
+        Assertions.assertEquals("C:\\Program Files\\Java\\jdk-25.0.2", systemProperties.getJavaHome());
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+
+        @Component
+        @Getter
+        public static class SystemProperties {
+
+            @Value("${JAVA_HOME}")
+            private String javaHome;
+
+        }
+
+        @Component
+        @Getter
+        public static class ApplicationProperties {
+
+            @Value("${spring.application.name}")
+            private String name;
+
+            @Value("${spring.application.version}")
+            private Integer version;
+
+            @Value("${spring.application.production}")
+            private boolean productionMode;
+
+        }
+
+    }
+
+}
