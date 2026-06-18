8abc40712a814c33fb9e51b86d4264ff4d6bf037
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-18 15:53:02 +0700

    Spring Aspect Oriented Programming 1 # Membuat Project

---
+++ belajar-spring-aop/src/test/java/bachtiar/firdaus/belajar_spring_aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
