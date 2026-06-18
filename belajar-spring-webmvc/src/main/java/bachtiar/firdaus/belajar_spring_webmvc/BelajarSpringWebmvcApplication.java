6a506e2300f259df2d51453404fac063117823a3
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-18 19:31:43 +0700

    Spring Web MVC 1 # Membuat Project

---
+++ belajar-spring-webmvc/src/main/java/bachtiar/firdaus/belajar_spring_webmvc/BelajarSpringWebmvcApplication.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringWebmvcApplication.class, args);
+	}
+
+}
---
+++ belajar-spring-webmvc/src/test/java/bachtiar/firdaus/belajar_spring_webmvc/BelajarSpringWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
