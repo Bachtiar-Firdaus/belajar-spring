9a7fa364f2a95deab6457fa89849ba9d4a30b800
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-18 19:39:02 +0700

    Spring Web MVC 3 # Request Mapping

--- belajar-spring-webmvc/src/main/java/bachtiar/firdaus/belajar_spring_webmvc/controller/HelloController.java
+++ belajar-spring-webmvc/src/main/java/bachtiar/firdaus/belajar_spring_webmvc/controller/HelloController.java
@@ -1,7 +1,15 @@
 package bachtiar.firdaus.belajar_spring_webmvc.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class HelloController {
+    @RequestMapping(path = "/hello")
+    public void helloWord(HttpServletResponse response) throws IOException{
+        response.getWriter().println("Hello World");
+    }
 }
