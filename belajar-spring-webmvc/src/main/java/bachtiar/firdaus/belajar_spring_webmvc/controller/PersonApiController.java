ccc1c2821fbfae933d5484d109cc1925599fb98b
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-20 19:50:51 +0700

    Spring Web MVC 22 # JSON

---
+++ belajar-spring-webmvc/src/main/java/bachtiar/firdaus/belajar_spring_webmvc/controller/PersonApiController.java
@@ -0,0 +1,23 @@
+package bachtiar.firdaus.belajar_spring_webmvc.controller;
+
+import bachtiar.firdaus.belajar_spring_webmvc.model.CreatePersonRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PersonApiController {
+
+    @PostMapping(
+            path = "/api/person",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public CreatePersonRequest createPerson(@RequestBody @Valid CreatePersonRequest request){
+        return request;
+    }
+}
