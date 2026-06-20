457fb395cb531c6ca51c5479383d512829bab463
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-20 17:19:17 +0700

    Spring Web MVC 13 # Request Header

---
+++ belajar-spring-webmvc/src/test/java/bachtiar/firdaus/belajar_spring_webmvc/controller/HeaderControllerTest.java
@@ -0,0 +1,45 @@
+package bachtiar.firdaus.belajar_spring_webmvc.controller;
+
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HeaderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void headerOk() throws Exception {
+        mockMvc.perform(
+                get("/header/token")
+                        .header("X-TOKEN", "EKO")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("OK"))
+        );
+    }
+
+    @Test
+    void headerKo() throws Exception {
+        mockMvc.perform(
+                get("/header/token")
+                        .header("X-TOKEN", "SALAH")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("KO"))
+        );
+    }
+}
