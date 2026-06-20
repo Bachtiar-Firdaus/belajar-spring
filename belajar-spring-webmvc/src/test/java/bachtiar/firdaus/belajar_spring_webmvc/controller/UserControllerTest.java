62ce5647c2f06608e96084b3513853de6e53f7f5
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-20 20:24:10 +0700

    Spring Web MVC 26 # Session Attribut

---
+++ belajar-spring-webmvc/src/test/java/bachtiar/firdaus/belajar_spring_webmvc/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package bachtiar.firdaus.belajar_spring_webmvc.controller;
+
+
+import bachtiar.firdaus.belajar_spring_webmvc.model.User;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUser() throws Exception {
+        mockMvc.perform(
+                get("/user/current")
+                        .sessionAttr("user", new User("eko"))
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello eko"))
+        );
+    }
+
+//    @Test
+//    public void getUserInvalid() throws Exception {
+//        mockMvc.perform(
+//                get("/user/current")
+//        ).andExpectAll(
+//                status().is3xxRedirection()
+//        );
+//    }
+
+}
