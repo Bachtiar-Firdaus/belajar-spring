34d8a56802320b1006ed2e31d244e21ca640037a
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-20 18:14:17 +0700

    Spring Web MVC 19 # Response Entity

---
+++ belajar-spring-webmvc/src/main/java/bachtiar/firdaus/belajar_spring_webmvc/model/User.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_webmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String username;
+}
