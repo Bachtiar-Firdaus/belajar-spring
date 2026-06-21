5022c3ec8646f02712fcbbe3a46f75e451487a11
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-21 17:25:21 +0700

    Spring Data JPA 8 # Programatic Transaction

--- belajar-spring-data-jpa/src/main/java/bachtiar/firdaus/belajar_spring_data_jpa/service/CategoryService.java
+++ belajar-spring-data-jpa/src/main/java/bachtiar/firdaus/belajar_spring_data_jpa/service/CategoryService.java
@@ -18,46 +18,46 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-//    @Autowired
-//    private TransactionOperations transactionOperations;
-//
-//    @Autowired
-//    private PlatformTransactionManager transactionManager;
-//
-//    public void manual(){
-//        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
-//        definition.setTimeout(10);
-//        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-//
-//        TransactionStatus transaction = transactionManager.getTransaction(definition);
-//        try {
-//            for (int i = 0; i < 5; i++) {
-//                Category category = new Category();
-//                category.setName("Category " + i);
-//                categoryRepository.save(category);
-//            }
-//            error();
-//            transactionManager.commit(transaction);
-//        }catch (Throwable throwable){
-//            transactionManager.rollback(transaction);
-//            throw throwable;
-//        }
-//    }
-//
-//    public void error(){
-//        throw new RuntimeException("Ups");
-//    }
-//
-//    public void createCategories(){
-//        transactionOperations.executeWithoutResult(transactionStatus -> {
-//            for (int i = 0; i < 5; i++) {
-//                Category category = new Category();
-//                category.setName("Category " + i);
-//                categoryRepository.save(category);
-//            }
-//            error();
-//        });
-//    }
+    @Autowired
+    private TransactionOperations transactionOperations;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    public void manual(){
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setTimeout(10);
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        TransactionStatus transaction = transactionManager.getTransaction(definition);
+        try {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category " + i);
+                categoryRepository.save(category);
+            }
+            error();
+            transactionManager.commit(transaction);
+        }catch (Throwable throwable){
+            transactionManager.rollback(transaction);
+            throw throwable;
+        }
+    }
+
+    public void error(){
+        throw new RuntimeException("Ups");
+    }
+
+    public void createCategories(){
+        transactionOperations.executeWithoutResult(transactionStatus -> {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category " + i);
+                categoryRepository.save(category);
+            }
+            error();
+        });
+    }
 
     @Transactional
     public void create(){
--- belajar-spring-data-jpa/src/test/java/bachtiar/firdaus/belajar_spring_data_jpa/service/CategoryServiceTest.java
+++ belajar-spring-data-jpa/src/test/java/bachtiar/firdaus/belajar_spring_data_jpa/service/CategoryServiceTest.java
@@ -27,17 +27,17 @@ class CategoryServiceTest {
         });
     }
 
-//    @Test
-//    void programmatic() {
-//        assertThrows(RuntimeException.class, () -> {
-//            categoryService.createCategories();
-//        });
-//    }
-//
-//    @Test
-//    void manual() {
-//        assertThrows(RuntimeException.class, () -> {
-//            categoryService.manual();
-//        });
-//    }
+    @Test
+    void programmatic() {
+        assertThrows(RuntimeException.class, () -> {
+            categoryService.createCategories();
+        });
+    }
+
+    @Test
+    void manual() {
+        assertThrows(RuntimeException.class, () -> {
+            categoryService.manual();
+        });
+    }
 }
