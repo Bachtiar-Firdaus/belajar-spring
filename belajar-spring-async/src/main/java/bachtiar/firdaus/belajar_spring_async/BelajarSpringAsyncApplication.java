78c1afedf6b0389d2b91e98dbc1df834dd8205e0
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-23 20:12:44 +0700

    Spring Async 2 # Enable Async

--- belajar-spring-async/src/main/java/bachtiar/firdaus/belajar_spring_async/BelajarSpringAsyncApplication.java
+++ belajar-spring-async/src/main/java/bachtiar/firdaus/belajar_spring_async/BelajarSpringAsyncApplication.java
@@ -2,8 +2,10 @@ package bachtiar.firdaus.belajar_spring_async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BelajarSpringAsyncApplication {
 
 	public static void main(String[] args) {
