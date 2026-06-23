d230ce97949a5b8d3dafc9331b044371a34d4fe8
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-23 17:36:50 +0700

    Spring RESTful API 6 # Address Entity

--- belajar-spring-restful-api/src/main/java/bachtiar/firdaus/belajar_spring_restful_api/entity/Contact.java
+++ belajar-spring-restful-api/src/main/java/bachtiar/firdaus/belajar_spring_restful_api/entity/Contact.java
@@ -33,6 +33,6 @@ public class Contact {
     @JoinColumn(name = "username", referencedColumnName = "username")
     private User user;
 
-//    @OneToMany(mappedBy = "contact")
-//    private List<Address> addresses;
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addresses;
 }
