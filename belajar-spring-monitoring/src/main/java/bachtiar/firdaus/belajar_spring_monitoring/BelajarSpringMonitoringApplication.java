354da130fd6df4b29261e508251f31b688c4d5b8
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-23 20:44:26 +0700

    Spring Monitoring 1 # Membuat Project

---
+++ belajar-spring-monitoring/src/main/java/bachtiar/firdaus/belajar_spring_monitoring/BelajarSpringMonitoringApplication.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringMonitoringApplication.class, args);
+	}
+
+}
---
+++ belajar-spring-monitoring/src/test/java/bachtiar/firdaus/belajar_spring_monitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
