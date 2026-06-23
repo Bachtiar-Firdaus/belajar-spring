691a6aaaa5e9cedae5f4845b5b26f271d37cc77c
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-23 18:07:36 +0700

    Spring RESTful API 7 # Register User Api

---
+++ belajar-spring-restful-api/src/main/java/bachtiar/firdaus/belajar_spring_restful_api/controller/UserController.java
@@ -0,0 +1,45 @@
+package bachtiar.firdaus.belajar_spring_restful_api.controller;
+
+import bachtiar.firdaus.belajar_spring_restful_api.entity.User;
+import bachtiar.firdaus.belajar_spring_restful_api.model.RegisterUserRequest;
+import bachtiar.firdaus.belajar_spring_restful_api.model.WebResponse;
+import bachtiar.firdaus.belajar_spring_restful_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(
+            path = "/api/users",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
+        userService.register(request);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+
+//    @GetMapping(
+//            path = "/api/users/current",
+//            produces = MediaType.APPLICATION_JSON_VALUE
+//    )
+//    public WebResponse<UserResponse> get(User user) {
+//        UserResponse userResponse = userService.get(user);
+//        return WebResponse.<UserResponse>builder().data(userResponse).build();
+//    }
+//
+//    @PatchMapping(
+//            path = "/api/users/current",
+//            consumes = MediaType.APPLICATION_JSON_VALUE,
+//            produces = MediaType.APPLICATION_JSON_VALUE
+//    )
+//    public WebResponse<UserResponse> update(User user, @RequestBody UpdateUserRequest request) {
+//        UserResponse userResponse = userService.update(user, request);
+//        return WebResponse.<UserResponse>builder().data(userResponse).build();
+//    }
+}
---
+++ belajar-spring-restful-api/src/main/java/bachtiar/firdaus/belajar_spring_restful_api/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package bachtiar.firdaus.belajar_spring_restful_api.repository;
+
+
+import bachtiar.firdaus.belajar_spring_restful_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findFirstByToken(String token);
+}
