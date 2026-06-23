db45db470e7c8582b68a5020cbd1097f80268add
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-23 19:47:02 +0700

    Spring RESTful API 20 # Delete Address Api

--- belajar-spring-restful-api/src/main/java/bachtiar/firdaus/belajar_spring_restful_api/service/AddressService.java
+++ belajar-spring-restful-api/src/main/java/bachtiar/firdaus/belajar_spring_restful_api/service/AddressService.java
@@ -92,23 +92,23 @@ public class AddressService {
         return toAddressResponse(address);
     }
 
-//    @Transactional
-//    public void remove(User user, String contactId, String addressId){
-//        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact is not found"));
-//
-//        Address address = addressRepository.findFirstByContactAndId(contact, addressId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Address is not found"));
-//
-//        addressRepository.delete(address);
-//    }
-//
-//    @Transactional(readOnly = true)
-//    public List<AddressResponse> list(User user, String contactId){
-//        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact is not found"));
-//
-//        List<Address> addresses = addressRepository.findAllByContact(contact);
-//        return addresses.stream().map(this::toAddressResponse).toList();
-//    }
+    @Transactional
+    public void remove(User user, String contactId, String addressId){
+        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact is not found"));
+
+        Address address = addressRepository.findFirstByContactAndId(contact, addressId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Address is not found"));
+
+        addressRepository.delete(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<AddressResponse> list(User user, String contactId){
+        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact is not found"));
+
+        List<Address> addresses = addressRepository.findAllByContact(contact);
+        return addresses.stream().map(this::toAddressResponse).toList();
+    }
 }
