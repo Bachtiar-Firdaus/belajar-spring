c6b5cdf7888b2130174e2989b68ee1ca46db610a
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-28 14:55:10 +0700

    Spring Data Redis 3 # Redis Template, Value Operation, List Operation

--- belajar-spring-redis/src/main/java/bachtiar/firdaus/belajar_spring_redis/BelajarSpringRedisApplication.java
+++ belajar-spring-redis/src/main/java/bachtiar/firdaus/belajar_spring_redis/BelajarSpringRedisApplication.java
@@ -2,8 +2,10 @@ package bachtiar.firdaus.belajar_spring_redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching; // Tambahkan import ini
 
 @SpringBootApplication
+@EnableCaching // Tambahkan anotasi ini di sini
 public class BelajarSpringRedisApplication {
 
 	public static void main(String[] args) {
