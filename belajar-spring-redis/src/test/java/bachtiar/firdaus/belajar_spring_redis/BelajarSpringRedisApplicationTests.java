3f4fad41518ad6c7f1301423b8d9ac8a01903141
bachtiar-firdaus <id.bachtiar.firdaus@gmail.com>
2026-06-28 14:13:45 +0700

    Spring Data Redis 1 # Membuat Project

---
+++ belajar-spring-redis/src/test/java/bachtiar/firdaus/belajar_spring_redis/BelajarSpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package bachtiar.firdaus.belajar_spring_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
